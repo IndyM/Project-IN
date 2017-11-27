c927ad46fad2c28e3e5a9fdecb933339a3b1579e
IndyM <123indym@gmail.com>
2017-11-27 02:39:53 +0100

    Project Fix

---
+++ Main/Model/Objects/MeshObject.cs
@@ -0,0 +1,86 @@
+﻿using DMS.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMS.Geometry;
+using System.IO;
+using DMS.Base;
+using System.Numerics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Model.Objects
+{
+    public class MeshObject : IMeshObject
+    {
+        public IMeshAttribute<Vector4> baseColor = new MeshAttribute<Vector4>(nameof(baseColor));
+        protected Mesh _mesh;
+        protected VAO _vao;
+        protected Shader _shader;
+
+
+        public Shader Shader
+        {
+            get { return _shader; }
+            set
+            {
+                _shader = value;
+                if (Mesh != null)
+                    Load();
+            }
+        }
+        public VAO Vao
+        {
+            get { return _vao; }
+            private set { _vao = value; }
+        }
+        public Mesh Mesh
+        {
+            get { return _mesh; }
+            set
+            {
+                _mesh = value;
+                foreach (var normal in Mesh.normal.List)
+                    baseColor.List.Add(new Vector4(normal, 1.0f));
+                if (Shader != null)
+                    Load();
+            }
+        }
+
+
+        public MeshObject()
+        {
+
+            var dir = Path.GetDirectoryName(PathTools.GetSourceFilePath()) + @"\..\Resources\Shader\";
+            Shader = ShaderLoader.FromFiles(dir + "vertex_base.glsl", dir + "frag_base.glsl");
+        }
+
+        public MeshObject(Mesh mesh) : this()
+        {
+            Mesh = mesh;
+        }
+
+        protected virtual void Load()
+        {
+            Vao = VAOLoader.FromMesh(Mesh, _shader);
+        }
+
+        public virtual void Render(OpenTK.Matrix4 camera)
+        {
+            if (Vao == null)
+            {
+                System.Diagnostics.Debug.WriteLine(nameof(MeshObject) + " Render, Vao null");
+                return;
+            }
+            //            GL.Disable(EnableCap.CullFace);
+            //GL.CullFace(CullFaceMode.FrontAndBack);
+
+            _shader.Activate();
+            _vao.SetAttribute(_shader.GetAttributeLocation(baseColor.Name), baseColor.List.ToArray(), VertexAttribPointerType.Float, 4);
+            GL.UniformMatrix4(_shader.GetUniformLocation("camera"), true, ref camera);
+            _vao.Draw();
+            _shader.Deactivate();
+        }
+    }
+}
---
+++ Main/Model/Objects/MeshObjectBP3DGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Objects
+{
+    public class MeshObjectBP3DGroup : MeshObjectBP3D , IObjectBP3DGroup
+    {
+        public ObservableCollection<IObjectBP3DGroup> Children
+        {
+            get;
+            set;
+        }
+
+        public bool IsMeshObject
+        {
+            get { return true; }
+        }
+
+        public MeshObjectBP3DGroup(MeshObjectBP3D meshObject) : base(meshObject)
+        {
+            Children = new ObservableCollection<IObjectBP3DGroup>();
+        }
+    }
+}
